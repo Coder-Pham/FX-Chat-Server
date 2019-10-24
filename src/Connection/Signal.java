e88cac0b09ea0f706ec581d580eba4c00c0bacae
Jay Huynh <duonghuynhthai239@gmail.com>
2019-10-24 07:28:51 +0700

    Add getter and setter for Signal

--- src/Connection/Signal.java
+++ src/Connection/Signal.java
@@ -23,11 +23,27 @@ public class Signal implements Serializable {
         this.action = action;
     }
 
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
     public Object getData() {
         return data;
     }
 
     public void setData(Object data) {
         this.data = data;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
     }
 }
