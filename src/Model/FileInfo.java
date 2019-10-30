5522defae17026a446c20a223079771d1aeabb1b
Quan Tran <quan.tran.itbk@hcmut.edu.vn>
2019-10-30 20:37:03 +0700

    update extend Model for FileInfo

--- src/Model/FileInfo.java
+++ src/Model/FileInfo.java
@@ -1,10 +1,6 @@
 package Model;
 
-import java.io.Serializable;
-
-public class FileInfo implements Serializable {
-    private static final long serialVersionUID = 1L;
-
+public class FileInfo extends Model {
     private User sender;
     private User receiver;
     private String filename;
